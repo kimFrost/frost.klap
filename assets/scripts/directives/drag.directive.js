26365422280a31043f7b792275786a6f528a2835
kimFrost <kim@thatworks.dk>
2015-03-17 18:05:41 +0100

    ()

---
+++ assets/scripts/directives/drag.directive.js
@@ -0,0 +1,110 @@
+﻿// shim layer with setTimeout fallback
+window.requestAnimFrame = (function(){
+	return  window.requestAnimationFrame       ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame    ||
+		function( callback ){
+			window.setTimeout(callback, 1000 / 60);
+		};
+})();
+
+(function (undefiend) {
+	'use strict';
+
+	angular
+		.module('noerd.Klap')
+		.directive('drag', ['$timeout', drag]);
+
+	function drag($timeout) {
+		var directive = {
+			replace: false,
+			require: '^dragContainer',
+			link: link,
+			scope: false,
+			restrict: 'A',
+			controller: controller,
+			controllerAs: 'drag',
+			bindToController: true
+		};
+		return directive;
+
+		function link(scope, element, attrs, controller) {
+
+			function updateLoop() {
+				window.requestAnimFrame(updateLoop);
+				update();
+			}
+
+			function update() {
+				var metric = controller.getMetric();
+				var offsets = element[0].getBoundingClientRect();
+				// Only force update if one of the metrics has changes
+				if (metric.pushFromTop != scope.drag.metric.pushFromTop || metric.height != scope.drag.metric.containerHeight || scope.drag.metric.height != offsets.height) {
+					scope.drag.metric.pushFromTop = metric.pushFromTop;
+					scope.drag.metric.containerHeight = metric.height;
+					scope.drag.metric.height = offsets.height;
+					updateData();
+				}
+			}
+
+			function updateData() {
+				var value = scope.drag.metric.pushFromTop;
+				var maxPush = scope.drag.metric.containerHeight - scope.drag.metric.height;
+				if (value > maxPush) {
+					value = maxPush;
+				}
+
+				// Js direct set style for much better performance.
+				element[0].style.mozTransform = 'translate(0px, '+ value +'px)';
+				element[0].style.msTransform = 'translate(0px, '+ value +'px)';
+				element[0].style.webkitTransform = 'translate(0px, '+ value +'px)';
+				element[0].style.OTransform  = 'translate(0px, '+ value +'px)';
+				element[0].style.transform = 'translate(0px, '+ value +'px)';
+
+				/*
+				var css = {
+					"-moz-transform": "translate(0px, "+ value +"px)",
+					"-ms-transform": "translate(0px, "+ value +"px)",
+					"-webkit-transform": "translate(0px, "+ value +"px)",
+					"transform": "translate(0px, "+ value +"px)"
+				};
+				scope.drag.css = css;
+				setTimeout(function(){
+					scope.$apply(); // Remove this for a better performance. But will not update on every tick
+				},0);
+				*/
+			}
+			updateLoop();
+		}
+
+		function controller() {
+			var drag = this;
+			drag.options = {};
+			drag.metric = {
+				top: null,
+				left: null,
+				pushFromTop: null,
+				height: null,
+				containerHeight: null
+			};
+			drag.states = {};
+			drag.temp = {};
+			drag.css = {};
+
+			// Function Library
+			// Visible
+			drag.visible = function(element) {
+				return (element.offsetWidth > 0 && element.offsetHeight > 0);
+				/*
+				if (element.offsetWidth > 0 && element.offsetHeight > 0) {
+					return true;
+				}
+				else {
+					return false;
+				}
+				*/
+			};
+
+		}
+	}
+})();
---
+++ assets/scripts/directives/dragcontainer.directive.js
@@ -0,0 +1,112 @@
+﻿// shim layer with setTimeout fallback
+window.requestAnimFrame = (function(){
+	return  window.requestAnimationFrame       ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame    ||
+		function( callback ){
+			window.setTimeout(callback, 1000 / 60);
+		};
+})();
+
+(function (undefiend) {
+	'use strict';
+
+	angular
+		.module('noerd.Klap')
+		.directive('dragContainer', ['$timeout', dragContainer]);
+
+	function dragContainer($timeout) {
+		var directive = {
+			replace: false,
+			link: link,
+			scope: false,
+			restrict: 'A',
+			controller: controller,
+			controllerAs: 'dragcon',
+			bindToController: true
+		};
+		return directive;
+
+		function link(scope, element, attrs) {
+
+			scope.dragcon.elem = element;
+
+			// Update Loop
+			function updateLoop() {
+				// Set next update call
+				window.requestAnimFrame(updateLoop);
+				// Update values
+				update();
+			}
+
+			// Update
+			function update() {
+				var el = element[0];
+				var offsets = el.getBoundingClientRect();
+				// Push from top distance
+				var topDiff = offsets.top;
+				// Self height
+				var height = offsets.height;
+				if (topDiff > 0) {
+					topDiff = 0;
+				}
+				topDiff = Math.abs(topDiff);
+				scope.dragcon.metric.height = height;
+				scope.dragcon.metric.pushFromTop = topDiff;
+				// Child component will self cal and monitor distance from bottom
+			}
+
+			function handlePressed(e) {
+				console.log('pressed');
+				scope.dragcon.states.pressed = true;
+			}
+
+			function handleRelease(e) {
+				console.log('release');
+				scope.dragcon.states.pressed = false;
+			}
+
+			function handleMove(e) {
+				if (scope.dragcon.states.pressed) {
+					e.preventDefault();
+					console.log('move');
+				}
+			}
+
+			updateLoop();
+
+			// Bind to input events
+			scope.dragcon.elem[0].addEventListener('touchstart', handlePressed);
+			scope.dragcon.elem[0].addEventListener('mousedown', handlePressed);
+			scope.dragcon.elem[0].addEventListener('touchend', handleRelease);
+			scope.dragcon.elem[0].addEventListener('mouseup', handleRelease);
+			scope.dragcon.elem[0].addEventListener('mouseleave', handleRelease);
+			scope.dragcon.elem[0].addEventListener('touchmove', handleMove);
+			scope.dragcon.elem[0].addEventListener('mousemove', handleMove);
+		}
+
+		// Inject dependecies to controller
+		controller.$inject = ['$element'];
+
+		function controller($element) {
+			var dragcon = this;
+			dragcon.options = {};
+			dragcon.metric = {
+				height: null,
+				pushFromTop: null
+			};
+			dragcon.elem = $element;
+			dragcon.states = {
+				pressed: false
+			};
+			dragcon.temp = {};
+			dragcon.css = {};
+
+			// Directive functions
+			dragcon.getMetric = function() {
+				return this.metric;
+			}
+		}
+	}
+})();
+
